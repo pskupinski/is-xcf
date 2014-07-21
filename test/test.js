c1c0fa0f00cfa3e91db13c204b5a06222973d41b
Preston Skupinski <preston.skupinski@gmail.com>
2014-07-20 22:49:34 -0500

    add test that verifies that it works with Uint8Arrays

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@
 var assert = require('assert');
 var isXCF = require('../index.js');
 var fs = require('fs');
+var xcfTestFile = fs.readFileSync('test/fixture.xcf');
 
 it('should reject a JPEG image from a buffer', function () {
   var jpegTestFile = fs.readFileSync('test/fixture.jpg');
@@ -9,6 +10,13 @@ it('should reject a JPEG image from a buffer', function () {
 });
 
 it('should detect an XCF image from a buffer', function () {
-  var xcfTestFile = fs.readFileSync('test/fixture.xcf');
   assert(isXCF(xcfTestFile));
+});
+
+it('should work with Uint8Arrays', function () {
+  var byteArray = new Uint8Array(xcfTestFile.length);
+  for (var i = 0; i < xcfTestFile.length; i++) {
+    byteArray[i] = xcfTestFile[i];
+  }
+  assert(isXCF(byteArray));
 });
