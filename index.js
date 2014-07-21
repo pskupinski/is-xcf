9a75f350c0cc61245aa96785feaa3fde0cc15aa7
Preston Skupinski <preston.skupinski@gmail.com>
2014-07-20 22:43:40 -0500

    Revert "make the xcf check a bit more readable"
    Definitely does not work with Uint8Arrays
    
    This reverts commit 845f6e2c9f8931d5b6af075280ee28785f032e6e.

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 module.exports = function (buf) {
   if (!buf || buf.length < 8) return false;
 
-  return /^gimp xcf/.test(buf);
+  // gimp xcf files start with "gimp xcf"
+  return buf[0] === 103 &&
+         buf[1] === 105 &&
+         buf[2] === 109 &&
+         buf[3] === 112 &&
+         buf[4] === 32 &&
+         buf[5] === 120 &&
+         buf[6] === 99 &&
+         buf[7] === 102;
 };
